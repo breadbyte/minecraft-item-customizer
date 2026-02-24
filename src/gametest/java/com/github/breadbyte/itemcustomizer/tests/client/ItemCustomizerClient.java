3b23586a85f1c4228b00813fcd60268292833488
breadbyte <14045257+breadbyte@users.noreply.github.com>
2026-02-25 05:46:06 +0800

    feat: initial testing framework

--- src/gametest/java/com/github/breadbyte/itemcustomizer/tests/client/ItemCustomizerClient.java
+++ src/gametest/java/com/github/breadbyte/itemcustomizer/tests/client/ItemCustomizerClient.java
@@ -2,19 +2,43 @@ package com.github.breadbyte.itemcustomizer.tests.client;
 
 import net.fabricmc.fabric.api.client.gametest.v1.FabricClientGameTest;
 import net.fabricmc.fabric.api.client.gametest.v1.context.ClientGameTestContext;
+import net.fabricmc.fabric.api.client.gametest.v1.context.TestServerContext;
 import net.fabricmc.fabric.api.client.gametest.v1.context.TestSingleplayerContext;
+import net.fabricmc.fabric.api.gametest.v1.GameTest;
+import net.minecraft.client.option.Perspective;
+import net.minecraft.client.world.GeneratorOptionsHolder;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.Items;
+import net.minecraft.resource.featuretoggle.FeatureSet;
+import net.minecraft.util.Hand;
+import net.minecraft.world.Difficulty;
+import net.minecraft.world.gen.GeneratorOptions;
+import net.minecraft.world.rule.GameRules;
 
 @SuppressWarnings("UnstableApiUsage")
 public class ItemCustomizerClient implements FabricClientGameTest {
+
+    final ItemStack DEFAULT_ITEM = Items.DIAMOND_SWORD.getDefaultStack();
+
     @Override
     public void runTest(ClientGameTestContext ctx) {
-        try (TestSingleplayerContext singleplayer = ctx.worldBuilder().create()) {
+        ctx.runOnClient(client -> {
+            client.options.setServerViewDistance(2);
+            client.options.setPerspective(Perspective.FIRST_PERSON);
+        });
+
+        try (TestSingleplayerContext singleplayer = ctx.worldBuilder()
+                .adjustSettings((c) -> {
+                    c.setDifficulty(Difficulty.PEACEFUL);
+                    c.setBonusChestEnabled(false);
+                }).create()) {
+
+            TestServerContext serverContext = singleplayer.getServer();
+            setup(serverContext, singleplayer);
             singleplayer.getClientWorld().waitForChunksRender();
-            ctx.takeScreenshot("example-mod-singleplayer-test");
-            ctx.getInput().typeChars("test");
-            ctx.wait();
-        } catch (InterruptedException e) {
-            throw new RuntimeException(e);
         }
+    }
+
+    public void setup(TestServerContext serverContext, TestSingleplayerContext singleplayerContext) {
     }
 }
--- src/gametest/java/com/github/breadbyte/itemcustomizer/tests/server/ItemCustomizerServer.java
+++ src/gametest/java/com/github/breadbyte/itemcustomizer/tests/server/ItemCustomizerServer.java
@@ -1,26 +1,55 @@
 package com.github.breadbyte.itemcustomizer.tests.server;
 
+import com.mojang.authlib.GameProfile;
+import com.mojang.brigadier.Command;
+import com.mojang.brigadier.LiteralMessage;
+import com.mojang.brigadier.Message;
+import com.mojang.brigadier.exceptions.CommandSyntaxException;
 import net.fabricmc.fabric.api.gametest.v1.CustomTestMethodInvoker;
 import net.fabricmc.fabric.api.gametest.v1.GameTest;
+import net.minecraft.component.DataComponentTypes;
+import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.Items;
+import net.minecraft.network.packet.c2s.common.SyncedClientOptions;
+import net.minecraft.server.command.CommandManager;
+import net.minecraft.server.command.CommandOutput;
+import net.minecraft.server.command.ServerCommandSource;
+import net.minecraft.server.network.ServerPlayerEntity;
 import net.minecraft.test.TestContext;
+import net.minecraft.text.Text;
 import net.minecraft.util.Hand;
+import net.minecraft.util.Identifier;
 import net.minecraft.world.GameMode;
 
 import java.lang.reflect.Method;
+import java.util.Objects;
+import java.util.UUID;
 
 public class ItemCustomizerServer implements CustomTestMethodInvoker {
-
-    @GameTest
-    public void CreateTestPlayer(TestContext context) {
-        var player = context.createMockPlayer(GameMode.DEFAULT);
-        player.setStackInHand(Hand.MAIN_HAND, Items.DIAMOND_SWORD.getDefaultStack());
-
-        context.assertTrue(player.getStackInHand(Hand.MAIN_HAND).getItem() == Items.DIAMOND_SWORD, "Player should have a diamond sword in hand");
-    }
-
     @Override
     public void invokeTestMethod(TestContext testContext, Method method) throws ReflectiveOperationException {
         method.invoke(this, testContext);
+    }
+
+    @GameTest
+    public void TestApplyModel(TestContext context) throws CommandSyntaxException {
+        var player = preinit(context);
+        executeCommand(context, player, "model apply minecraft stone");
+        context.assertEquals(
+                Identifier.of("minecraft", "stone"),
+                player.getMainHandStack().getComponents().get(DataComponentTypes.ITEM_MODEL),
+                Text.empty());
+    }
+    private PlayerEntity preinit(TestContext context) {
+        GameProfile profile = new GameProfile(UUID.randomUUID(), "TestPlayer");
+        ServerPlayerEntity s = new ServerPlayerEntity(context.getWorld().getServer(), context.getWorld(), profile, SyncedClientOptions.createDefault());
+        s.setStackInHand(Hand.MAIN_HAND, Items.DIAMOND_SWORD.getDefaultStack());
+
+        return s;
+    }
+
+    private void executeCommand(TestContext testContext, PlayerEntity player, String command) throws CommandSyntaxException {
+        var scs = player.getCommandSource(testContext.getWorld());
+        scs.getDispatcher().execute(command, scs);
     }
 }
