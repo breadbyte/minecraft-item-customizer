46ccb6d9b5c33868865e59f190361baed4e18aba
breadbyte <14045257+breadbyte@users.noreply.github.com>
2026-04-01 02:57:26 +0800

    fix: model namespace adapter is not using proper param names and returning an error on clear

--- src/main/java/com/github/breadbyte/itemcustomizer/server/commands/impl/model/namespace/ModelNamespaceAdapter.java
+++ src/main/java/com/github/breadbyte/itemcustomizer/server/commands/impl/model/namespace/ModelNamespaceAdapter.java
@@ -2,6 +2,7 @@ package com.github.breadbyte.itemcustomizer.server.commands.impl.model.namespace
 
 import com.github.breadbyte.itemcustomizer.server.commands.defs.Adapter;
 import com.github.breadbyte.itemcustomizer.server.commands.defs.model.namespace.ModelNamespaceParams;
+import com.github.breadbyte.itemcustomizer.server.commands.registry.builder.model.ModelNamespaceCommand;
 import com.github.breadbyte.itemcustomizer.server.util.Result;
 import com.mojang.brigadier.context.CommandContext;
 import net.minecraft.server.command.ServerCommandSource;
@@ -9,9 +10,16 @@ import net.minecraft.server.command.ServerCommandSource;
 public class ModelNamespaceAdapter implements Adapter<ModelNamespaceParams> {
     @Override
     public Result<ModelNamespaceParams> getParams(CommandContext<ServerCommandSource> ctx) {
-        var paramUrl = String.valueOf(ctx.getArgument("url", String.class));
-        var paramNamespace = String.valueOf(ctx.getArgument("namespace", String.class));
+        String paramUrl;
+        String paramNamespace;
 
+        try {
+            paramUrl = String.valueOf(ctx.getArgument(ModelNamespaceCommand.URL_ARGUMENT, String.class));
+            paramNamespace = String.valueOf(ctx.getArgument(ModelNamespaceCommand.NAMESPACE_ARGUMENT, String.class));
+        } catch (Exception e) {
+            // Clear doesn't need these params, so we can just return an empty params object
+            return Result.ok();
+        }
         return Result.ok(new ModelNamespaceParams(paramUrl, paramNamespace, ctx.getSource().getServer(), ctx.getSource()));
     }
 }
